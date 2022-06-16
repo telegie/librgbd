672a20ad96d90a7c3f04a2ff4ea4047078caf91e
Hanseul Jun <hanseuljun@gmail.com>
2022-06-15 20:31:01 -0700

    Use cxxopts for cli and print help and version.

--- src/cli/rgbd_cli.cpp
+++ src/cli/rgbd_cli.cpp
@@ -1,6 +1,21 @@
 #include <rgbd/video_parser.hpp>
+#include <cxxopts.hpp>
 
-int main() {
-    spdlog::info("hey");
+int main(int argc, char** argv)
+{
+    cxxopts::Options options{"rgbd-cli", "CLI for librgbd."};
+    options.add_option("", cxxopts::Option{"h,help", "Print Usage"});
+    options.add_option("", cxxopts::Option{"v,version", "Print Version"});
+
+    auto result{options.parse(argc, argv)};
+    if (result.count("help")) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    } else if (result.count("version")) {
+        std::cout << fmt::format("{}.{}.{}", 1, 5, 12) << std::endl;
+        return 0;
+    }
+
+    std::cout << "no option found from rgbd-cli" << std::endl;
     return 0;
 }
