496a4712bfc9712dcab2bc4459f73cc99a440149
Hanseul Jun <hanseuljun@gmail.com>
2022-08-05 13:51:35 -0700

    Open a function for testing kinfu.

--- include/rgbd/kinect_calibration_utils.hpp
+++ include/rgbd/kinect_calibration_utils.hpp
@@ -8,5 +8,10 @@
 
 namespace rgbd
 {
+// kinect_transformation_unproject opened only for kinfu testing.
+// TODO: hide this.
+glm::vec3 kinect_transformation_unproject(const KinectCameraCalibration& calibration,
+                                          const glm::vec2& point2d,
+                                          bool& valid);
 glm::vec3 compute_kinect_direction(const KinectCameraCalibration& calibration, const glm::vec2& uv);
 } // namespace tg
