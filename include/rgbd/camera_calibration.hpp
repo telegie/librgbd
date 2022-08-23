c96e2fa01eae9c9a6b48b24739ee9787bdbddc73
Hanseul Jun <hanseuljun@gmail.com>
2022-08-23 15:38:22 -0700

    add comment

--- include/rgbd/camera_calibration.hpp
+++ include/rgbd/camera_calibration.hpp
@@ -15,6 +15,7 @@ public:
     virtual int getColorHeight() const noexcept = 0;
     virtual int getDepthWidth() const noexcept = 0;
     virtual int getDepthHeight() const noexcept = 0;
+    // The range of uv would be [0, 1] x [0, 1]
     virtual glm::vec3 getDirection(const glm::vec2& uv) const noexcept = 0;
 };
 }
