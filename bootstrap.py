d33e43011dcc30313808be17c3cbce325d5bc1a5
Hanseul Jun <hanseuljun@gmail.com>
2022-11-14 17:32:21 -0800

    Add bootstrap.py.

---
+++ bootstrap.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import subprocess
+from pathlib import Path
+
+
+def main():
+    here = Path(__file__).parent.resolve()
+    subprocess.run(["python3", f"{here}/deps/libpng-binaries/build.py"])
+
+
+if __name__ == "__main__":
+    main()
