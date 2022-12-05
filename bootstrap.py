49f1bb0db538ab40e032c9022d1903004ae77722
Hanseul Jun <hanseuljun@gmail.com>
2022-12-05 15:28:42 -0800

    Remove building ffmpeg from bootstrap.py and just use the prebuilt binaires.

--- bootstrap.py
+++ bootstrap.py
@@ -5,7 +5,6 @@ from pathlib import Path
 
 def main():
     here = Path(__file__).parent.resolve()
-    subprocess.run(["python3", f"{here}/deps/ffmpeg-binaries/build.py"], check=True)
     subprocess.run(["python3", f"{here}/deps/libpng-binaries/build.py"], check=True)
 
 
