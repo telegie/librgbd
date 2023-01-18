e8940d692d7590294062e0fa28bf775d288fde49
hanseuljun <hanseuljun@gmail.com>
2023-01-17 18:37:28 -0800

    Clean up test_encoder.py code.

--- python/test_encoder.py
+++ python/test_encoder.py
@@ -17,28 +17,10 @@ def main():
         with open(video_file_path, "wb+") as file:
             file.write(response.content)
 
-    # with rgbd.NativeFileParser(video_file_path) as native_file_parser:
-    #     with native_file_parser.parse_all_frames() as native_file:
-    #         file = rgbd.File(native_file)
-    #         directions = rgbd.get_calibration_directions(native_file)
-    #         with native_file.get_attachments() as native_attachments:
-    #             with native_attachments.get_camera_calibration() as native_calibration:
-    #                 write_config = rgbd.NativeFileWriterConfig()
-    #                 write_config.set_depth_codec_type(rgbd.lib.RGBD_DEPTH_CODEC_TYPE_TDC1)
-    #                 file_writer = rgbd.NativeFileWriter("tmp/written_file.mkv",
-    #                                                     native_calibration,
-    #                                                     write_config)
-
     with rgbd.NativeFileParser(video_file_path) as native_file_parser:
         with native_file_parser.parse(True, True) as native_file:
             file = rgbd.File(native_file)
 
-    # with file.attachments.camera_calibration.create_native_instance() as native_calibration:
-    #     write_config = rgbd.NativeFileWriterConfig()
-    #     write_config.set_depth_codec_type(rgbd.lib.RGBD_DEPTH_CODEC_TYPE_TDC1)
-    #     file_writer = rgbd.NativeFileWriter("tmp/written_file.mkv",
-    #                                         native_calibration,
-    #                                         write_config)
     file_writer_helper = rgbd.FileWriterHelper()
     file_writer_helper.set_calibration(file.attachments.camera_calibration)
 
@@ -63,7 +45,6 @@ def main():
             color_array = rgbd.convert_yuv420_to_rgb(yuv_frame.y_channel, yuv_frame.u_channel, yuv_frame.v_channel)
             yuv_frames.append(yuv_frame)
             color_arrays.append(color_array)
-    file_writer_helper.set_cover(yuv_frames[0])
 
     depth_frames = []
     with rgbd.NativeDepthDecoder(rgbd.DepthCodecType.TDC1) as depth_decoder:
@@ -79,23 +60,7 @@ def main():
 
     print(f"depth_width: {depth_width}, depth_height: {depth_height}")
 
-    audio_frame_index = 0
-    imu_frame_index = 0
-
-    # minimum_time_point_us = file.video_frames[0].time_point_us
-    # if len(file.audio_frames):
-    #     minimum_time_point_us = min(minimum_time_point_us, file.audio_frames[0].time_point_us)
-    # if len(file.imu_frames):
-    #     minimum_time_point_us = min(minimum_time_point_us, file.imu_frames[0].time_point_us)
-
-    for audio_frame in file.audio_frames:
-        file_writer_helper.add_audio_frame(audio_frame)
-
-    for imu_frame in file.imu_frames:
-        file_writer_helper.add_imu_frame(imu_frame)
-
-    for trs_frame in file.trs_frames:
-        file_writer_helper.add_trs_frame(trs_frame)
+    file_writer_helper.set_cover(yuv_frames[0])
 
     with rgbd.NativeColorEncoder(rgbd.lib.RGBD_COLOR_CODEC_TYPE_VP8,
                                  yuv_frame.width,
@@ -106,43 +71,12 @@ def main():
         for index in range(len(file.video_frames)):
             video_frame = file.video_frames[index]
             keyframe = index % 60 == 0
-            # print(f"index: {index}, keyframe: {keyframe}")
-
-            # # Write audio frames fitting in front of the video frame.
-            # while audio_frame_index < len(file.audio_frames):
-            #     audio_frame = file.audio_frames[audio_frame_index]
-            #     if audio_frame.time_point_us > video_frame.time_point_us:
-            #         break
-            #     file_writer.write_audio_frame(audio_frame.time_point_us - minimum_time_point_us,
-            #                                   rgbd.cast_np_array_to_pointer(audio_frame.bytes),
-            #                                   audio_frame.bytes.size)
-            #     audio_frame_index = audio_frame_index + 1
-
-            # # Write IMU frames fitting in front of the video frame.
-            # while imu_frame_index < len(file.imu_frames):
-            #     imu_frame = file.imu_frames[imu_frame_index]
-            #     if imu_frame.time_point_us > video_frame.time_point_us:
-            #         break
-            #     file_writer.write_imu_frame(imu_frame.time_point_us - minimum_time_point_us,
-            #                                 imu_frame.acceleration,
-            #                                 imu_frame.rotation_rate,
-            #                                 imu_frame.magnetic_field,
-            #                                 imu_frame.gravity)
-            #     imu_frame_index = imu_frame_index + 1
 
             yuv_frame = yuv_frames[index]
-
-            # if index == 0:
-            #     file_writer.write_cover(yuv_frame)
-
             depth_frame = depth_frames[index]
             color_bytes = color_encoder.encode(yuv_frame, keyframe)
             depth_bytes = depth_encoder.encode(depth_frame.values, keyframe)
 
-            # file_writer.write_video_frame(video_frame.time_point_us - minimum_time_point_us,
-            #                               keyframe,
-            #                               color_bytes,
-            #                               depth_bytes)
             file_writer_helper.add_color_byte_frame(video_frame.time_point_us,
                                                     keyframe,
                                                     color_bytes)
@@ -150,12 +84,15 @@ def main():
                                                     keyframe,
                                                     depth_bytes)
 
-            # file_writer.write_trs_frame(video_frame.time_point_us - minimum_time_point_us,
-            #                             glm.vec3(0, 0, 0),
-            #                             glm.quat(1, 0, 0, 0),
-            #                             glm.vec3(1, 2, 1))
+    for audio_frame in file.audio_frames:
+        file_writer_helper.add_audio_frame(audio_frame)
 
-    # file_writer.flush()
+    for imu_frame in file.imu_frames:
+        file_writer_helper.add_imu_frame(imu_frame)
+
+    for trs_frame in file.trs_frames:
+        file_writer_helper.add_trs_frame(trs_frame)
+
     file_writer_helper.write("tmp/written_file.mkv")
 
 
