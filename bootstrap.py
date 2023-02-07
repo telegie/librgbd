d0230d426f497dbc4e24129364d8f297dd81f34d
Hanseul Jun <hanseuljun@gmail.com>
2023-02-07 19:43:38 +0100

    Install lzma for linux in bootstrap.

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import platform
 import subprocess
 import sys
 from pathlib import Path
@@ -7,6 +8,10 @@ from pathlib import Path
 def main():
     here = Path(__file__).parent.resolve()
     subprocess.run(["python3", f"{here}/deps/libpng-binaries/build.py"] + sys.argv[1:], check=True)
+    if platform.system() == "Linux":
+        subprocess.run(["sudo", "apt", "install", "-y",
+                        "liblzma-dev"],
+                       check=True)
 
 
 if __name__ == "__main__":
