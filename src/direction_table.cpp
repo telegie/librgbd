df32c584583bbf40d617ca55828d828502e5f660
Hanseul Jun <hanseuljun@gmail.com>
2023-02-22 14:19:42 -0800

    Fix DirectionTable::getDirection().

--- src/direction_table.cpp
+++ src/direction_table.cpp
@@ -21,12 +21,13 @@ DirectionTable::DirectionTable(const rgbd::CameraCalibration& calibration)
 {
     directions_.resize(gsl::narrow<size_t>(width_ * height_));
 
+    float u_scale{1.0f / (width_ - 1)};
+    float v_scale{1.0f / (height_ - 1)};
     for (int row{0}; row < height_; ++row) {
         for (int col{0}; col < width_; ++col) {
-            float u{col / static_cast<float>(width_ - 1)};
-            float v{row / static_cast<float>(height_ - 1)};
-            directions_[col + static_cast<int64_t>(row) * width_] =
-                calibration.getDirection(glm::vec2{u, v});
+            float u{col * u_scale};
+            float v{row * v_scale};
+            directions_[col + row * width_] = calibration.getDirection(glm::vec2{u, v});
         }
     }
 }
@@ -35,8 +36,8 @@ glm::vec3 DirectionTable::getDirection(const glm::vec2& uv) const
 {
     // The row and col aiming to obtain,
     // and will be obtained through interpolation.
-    float col{uv.x * width_};
-    float row{uv.y * height_};
+    float col{uv.x * (width_ - 1)};
+    float row{uv.y * (height_ - 1)};
 
     int left_col{static_cast<int>(std::floor(col))};
     int top_row{static_cast<int>(std::floor(row))};
