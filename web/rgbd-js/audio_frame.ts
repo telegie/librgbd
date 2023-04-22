2df1b7ebcd8dd26eadbbaa15894de088dcb7cbdf
Hanseul Jun <hanseuljun@gmail.com>
2023-04-22 11:54:18 -0700

    Remove rgbd-js.

--- web/rgbd-js/audio_frame.ts
+++
@@ -1,8 +0,0 @@
-export class AudioFrame {
-  timePointUs: number;
-  pcmSamples: number[];
-  constructor(timePointUs: number, pcmSamples: number[]) {
-    this.timePointUs = timePointUs;
-    this.pcmSamples = pcmSamples;
-  }
-}
--- web/rgbd.ts
+++
@@ -1,3 +0,0 @@
-import * as RGBD from './rgbd-js/index';
-
-export default RGBD;
